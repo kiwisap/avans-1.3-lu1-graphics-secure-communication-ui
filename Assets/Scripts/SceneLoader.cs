8f9ee6f63414ef63476720765a58d25efd730079
JorisdeVos <jorisdevos0@gmail.com>
2026-03-30 15:17:00 +0200

    Finalized register login screen

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,10 +1,71 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    [Header("UI")]
+    public Image fadePanel;
+    public float fadeDuur = 1f;
+
+    void Start()
     {
+        // Fade in bij opstarten
+        StartCoroutine(FadeIn());
+    }
+
+    public void OnKindGekozen()
+    {
+        PlayerPrefs.SetString("GebruikerType", "kind");
+        PlayerPrefs.Save();
+        StartCoroutine(FadeNaarScene("WorldOverview"));
+    }
+
+    public void OnOuderGekozen()
+    {
+        PlayerPrefs.SetString("GebruikerType", "ouder");
+        PlayerPrefs.Save();
+        StartCoroutine(FadeNaarScene("WorldOverview"));
+    }
+
+    IEnumerator FadeIn()
+    {
+        // Start zwart, fade naar transparant
+        Color kleur = fadePanel.color;
+        kleur.a = 1f;
+        fadePanel.color = kleur;
+
+        float timer = 0f;
+        while (timer < fadeDuur)
+        {
+            timer += Time.deltaTime;
+            kleur.a = 1f - (timer / fadeDuur);
+            fadePanel.color = kleur;
+            yield return null;
+        }
+
+        kleur.a = 0f;
+        fadePanel.color = kleur;
+        fadePanel.raycastTarget = false;
+    }
+
+    IEnumerator FadeNaarScene(string sceneName)
+    {
+        fadePanel.raycastTarget = true;
+        Color kleur = fadePanel.color;
+        kleur.a = 0f;
+        fadePanel.color = kleur;
+
+        float timer = 0f;
+        while (timer < fadeDuur)
+        {
+            timer += Time.deltaTime;
+            kleur.a = timer / fadeDuur;
+            fadePanel.color = kleur;
+            yield return null;
+        }
+
         SceneManager.LoadScene(sceneName);
     }
 }
